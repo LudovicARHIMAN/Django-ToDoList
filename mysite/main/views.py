758c36cb7551c40fc5abbd1cbffb326ca10f870c
LudovicARHIMAN <ludovicarhiman900@gmail.com>
2024-04-22 15:15:22 +0400

    add django-for-beginners into repo

---
+++ mysite/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(response):
+    return HttpResponse("<h1>Hello World!</h1>")
+
+def view1(response):
+    return HttpResponse("<h1>View1 !</h1>")
