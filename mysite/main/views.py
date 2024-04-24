4832fcfa074ec4e45b3dfbdc28c9670523d46851
LudovicARHIMAN <ludovicarhiman900@gmail.com>
2024-04-24 14:17:18 +0400

    Add item into lists

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
+from django.views import View
 
-# Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
     return render(response, 'main/list.html', {"ls":ls})
@@ -19,6 +19,9 @@ def create(response):
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
+        return HttpResponseRedirect("/%i"  %t.id)
     else:
         form = CreateNewList()
     return render(response, 'main/create.html', {"form":form})
+
+
